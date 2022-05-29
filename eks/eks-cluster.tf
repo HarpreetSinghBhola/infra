912c2e18a009f92d8b624e34ec6550c59fdf2d26
Harpreet Singh <harpreet.singh7@hotmail.com>
2022-05-29 02:12:56 -0400

    Update eks-cluster.tf

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,32 +1,53 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
+  version         = "~> 18.0"
   cluster_name    = local.cluster_name
+<<<<<<< HEAD
+  cluster_version = "1.22"
+  subnet_ids      = module.vpc.private_subnets
+=======
   cluster_version = "1.20"
   subnets         = module.vpc.private_subnets
+>>>>>>> 89f16296dc3bd68bc3a042085ca9190572365616
+
+  cluster_endpoint_private_access  = true
+
+  vpc_id = module.vpc.vpc_id
+  
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::970247663978:user/jenkins"
+      username = "jenkins"
+      groups   = ["system:masters"] },
+  ]
+
+  eks_managed_node_groups = {
+    managed_node = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large", "t2.medium"]
+      capacity_type  = "SPOT"
+    }
+  }
 
   tags = {
     Environment = "${terraform.workspace}-eks"
     Terraform   = "true"    
   }
-
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-      asg_min_size                  = 2
-      asg_max_size                  = 3
-    },
-  ]
 }
 
 data "aws_eks_cluster" "cluster" {
